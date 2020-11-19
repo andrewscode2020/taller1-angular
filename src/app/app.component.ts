41a1b4f4dbe154e2fe593eb40e03285d4754c566
andrewscode2020 <greatandres@gmail.com>
2020-11-19 16:01:12 -0500

    Desarrollo del taller1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,31 @@ export class AppComponent {
     nombre: '',
     saldo: 0
   };
+  ingreso = false;
 
   abrirCuenta() {
-    console.log(this.cliente.nombre);
-    console.log(this.cliente.saldo);
-    if (this.cliente.nombre !== '' && this.cliente.saldo >= 0) {
-      let ingreso = true;
+    if (this.cliente.nombre !== '' && this.cliente.saldo > 0) {
+      return this.ingreso = true;
     } else if (this.cliente.nombre === '') {
       alert('Debes ingresar un nombre.');
     } else {
-      alert('Debes ingresar un saldo mayor o igual a cero.');
+      alert('Debes ingresar un saldo mayor a cero.');
     }
   }
 
+  consignar() {
+    let consignarValor = parseInt(prompt('Ingrese valor a consignar:'))
+    this.cliente.saldo += consignarValor
+  }
+
+  retirar() {
+    let retirarValor = parseInt(prompt('Ingrese valor a consignar:'))
+    this.cliente.saldo -= retirarValor
+  }
+
+  salir() {
+    this.ingreso = false;
+    this.cliente.nombre = '';
+    this.cliente.saldo = 0;
+  }
 }
